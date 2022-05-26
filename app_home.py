ba13183afe20ca29c1f4e48dfec4dfd978808225
eyoo95 <105832377+eyoo95@users.noreply.github.com>
2022-05-26 15:15:13 +0900

    add

--- app_home.py
+++ app_home.py
@@ -1,5 +1,21 @@
 import streamlit as st
 
 def run_home():
-    st.subheader('이 앱은 고객 데이터와 자동차 구매에 대한 내용을 담고 있습니다.')
-    st.text('왼쪽 사이드바에서 원하는 내용을 선택하세요')
+    st.subheader('왼쪽 사이드바에서 원하는 내용을 선택하세요.')
+    st.text('이 앱은 간단한 기상정보를 사용하여 교통사고를 예측할수 있는 앱입니다.')
+    st.subheader("")
+    st.info("사용한 데이터:\n1. [종관기상] 지점별 연·월 통계\n2. 시・도별 도로보급률\n3. 시도별 교통사고\n4. 도로교통사고 사망률")
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+
+
+    st.text('출처:\nhttps://kosis.kr/statHtml/statHtml.do?orgId=132&tblId=DT_V_MOTA_016\nhttps://www.index.go.kr/unify/idx-info.do?idxCd=4261')
+
